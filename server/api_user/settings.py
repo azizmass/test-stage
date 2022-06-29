5171bd2b4ac58ac0b12fe1910ba15b12973adb9a
Aziz Masmoudi <93921997+azizmass@users.noreply.github.com>
2022-06-29 03:26:01 +0100

    Update settings.py

--- server/api_user/settings.py
+++ server/api_user/settings.py
@@ -148,5 +148,5 @@ EMAIL_USE_TLS=True
 STATIC_URL='/static/'
 EMAIL_HOST='smtp.office365.com'
 EMAIL_PORT=587
-EMAIL_HOST_USER='azizmasmoudi019@outlook.com'
-EMAIL_HOST_PASSWORD='azerty112'
+EMAIL_HOST_USER=os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD=os.environ.get("EMAIL_HOST_PASSWORD")
